c4605c41fc8e3f193092a6eb02c68a91eb7b16ec
kechin.vv <kechinv1@rambler.ru>
2021-02-18 23:48:38 +0300

    intermediate update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,43 +12,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 
 public class Main {
 
+
     public static void main(String[] args) throws Exception {
         Main m = new Main();
         m.reader();
-   }
-
-    public void reader() throws Exception {
-        List<File> list = new ArrayList<>();
-        Files.walk(Paths.get("C:\\Users\\valer\\IdeaProjects\\KotlinAsFirst2019\\src"), FileVisitOption.FOLLOW_LINKS)
-                .map(Path::toFile)
-                .forEach(f -> {
-                    if (f.isFile()) list.add(f);
-                });
-        list.removeIf(f -> !f.getName().endsWith(".kt"));
-        System.out.println(list);
-        startParse(list.get(0));
     }
 
-    public static void startParse(File file) throws Exception{
-        KotlinLexer lexer = new KotlinLexer(new ANTLRFileStream(file.getAbsolutePath()));
-        TokenStream tokens = new CommonTokenStream(lexer);
-        KotlinParser parser = new KotlinParser(tokens);
-        printTree(parser.kotlinFile(), 0, lexer);
-    }
-
-    private static void printTree(ParseTree tree, int indentation, KotlinLexer lexer) throws Exception{
+    private static void printTree(ParseTree tree, int indentation, KotlinLexer lexer) throws Exception {
         printIndentation(indentation);
         String treeClassName = tree.getClass().getSimpleName();
         if (Objects.equals(treeClassName, "TerminalNodeImpl")) {
-            TerminalNodeImpl node = (TerminalNodeImpl)tree;
+            TerminalNodeImpl node = (TerminalNodeImpl) tree;
             printNode(node, lexer);
-        }
-        else {
+        } else {
             printRule(tree);
             if (tree.getChildCount() == 0) {
                 printIndentation(indentation);
@@ -60,7 +42,7 @@ public class Main {
         }
     }
 
-    private static void printIndentation(int indentation) throws Exception{
+    private static void printIndentation(int indentation) throws Exception {
         String output = "";
         for (int i = 0; i < indentation; i++) {
             output += "  ";
@@ -68,20 +50,96 @@ public class Main {
         System.out.print(output);
     }
 
-    private static void printNode(TerminalNodeImpl node, KotlinLexer lexer) throws Exception{
+    private static void printNode(TerminalNodeImpl node, KotlinLexer lexer) throws Exception {
         String nodeType = lexer.getVocabulary().getSymbolicName(getNodeTypeNumber(node));
         String nodeText = node.getText();
         if (Objects.equals(nodeType, "NL")) nodeText = "\\n";
         System.out.println("PsiElement" + "(" + nodeType + ")" + "('" + nodeText + "')");
     }
 
-    private static int getNodeTypeNumber(TerminalNodeImpl node) throws Exception{
+    private static int getNodeTypeNumber(TerminalNodeImpl node) throws Exception {
         return node.getSymbol().getType();
     }
 
-    private static void printRule(ParseTree tree) throws Exception{
+    private static void printRule(ParseTree tree) throws Exception {
         char[] ruleName = tree.getClass().getSimpleName().replaceFirst("Context", "").toCharArray();
         ruleName[0] = Character.toLowerCase(ruleName[0]);
         System.out.println(ruleName);
+    }
+
+    public void startParse(File file) throws Exception {
+        boolean fun = false;
+        boolean val = false;
+        int override = 0;
+        int fields = 0;
+        boolean wait = false;
+        int bracket = 0;
+        int a = 0;
+        int c = 0;
+        HashSet<String> operatorsA = new HashSet<String>(List.of("++", "--", "=", "*=", "/=", "%=", "+=", "-=", "<<=", ">>=", "&=", "!=", "^=", ">>>="));
+        HashSet<String> operatorsC = new HashSet<String>(List.of("==", "!=", ">=", "<=", ">", "<", "!", "else", "if", "?", "try", "catch", "when"));
+        //  int
+        KotlinLexer lexer = new KotlinLexer(new ANTLRFileStream(file.getAbsolutePath()));
+        TokenStream tokens = new CommonTokenStream(lexer);
+        KotlinParser parser = new KotlinParser(tokens);
+        KotlinParser.KotlinFileContext tree = parser.kotlinFile();
+        for (int i = 1; i < tokens.size(); i++) {
+            String prevToken = tokens.get(i - 1).getText();
+            String currToken = tokens.get(i).getText();
+            String currTokenType = lexer.getVocabulary().getDisplayName(tokens.get(i).getType());
+            if (prevToken.equals("class") && currTokenType.equals("Identifier"))
+                System.out.println("class " + currToken);
+            if (currToken.equals("override")) {
+                System.out.println("override " + currToken);
+                override++;
+            }
+
+            if ((currToken.equals("val") || currToken.equals("var") || currToken.equals("const")) && operatorsA.contains(tokens.get(i + 2).getText())) {
+                val = true;
+            }
+            if (currToken.equals("fun")) {
+                fun = true;
+                wait = true;
+            }
+            if (fun) {
+                if (currTokenType.equals("{")) {
+                    wait = false;
+                    bracket++;
+                }
+                if (currTokenType.equals("}")) {
+                    bracket--;
+                    if (!wait && bracket == 0) fun = false;
+                }
+            }
+
+            if (operatorsC.contains(currToken)) {
+                c++;
+            }
+
+            if ((currToken.equals("var") || currToken.equals("val") || currToken.equals("const")) && !fun) {
+                fields++;
+            }
+            if (operatorsA.contains(currToken) && !val) {
+                a++;
+            }
+            if (val && operatorsA.contains(currToken)) {
+                val = false;
+            }
+            System.out.println("token " + currToken + " tokentype " + currTokenType);
+        }
+        // printTree(tree, 0, lexer);
+        System.out.println("fields=" + fields + " override=" + override + " a metric=" + a+" c metric=" + c);
+    }
+
+    public void reader() throws Exception {
+        List<File> list = new ArrayList<>();
+        Files.walk(Paths.get("C:\\Users\\valer\\IdeaProjects\\forparse\\src"), FileVisitOption.FOLLOW_LINKS)
+                .map(Path::toFile)
+                .forEach(f -> {
+                    if (f.isFile()) list.add(f);
+                });
+        list.removeIf(f -> !f.getName().endsWith(".kt"));
+        System.out.println(list);
+        startParse(list.get(0));
     }
 }

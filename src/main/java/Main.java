fdb3433ee5e7989a5d27bf99a0d68e0055ad96d2
kechin.vv <kechinv1@rambler.ru>
2021-02-19 14:43:30 +0300

    All metric params added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -66,8 +66,8 @@ public class Main {
 
 
     public void startParse(File file) throws Exception {
-        String key="";
-        String value="";
+        String key = "";
+        String value = "";
         boolean fun = false;
         boolean val = false;
         boolean wait = false;
@@ -76,8 +76,10 @@ public class Main {
         int fields = 0;
         int bracket = 0;
         int a = 0;
+        int b = 0;
         int c = 0;
-        HashMap<String,String> extend = new HashMap<>();
+        //ArrayList<String> identifiers = new ArrayList<>();
+        HashMap<String, String> extend = new HashMap<>();
         HashSet<String> operatorsA = new HashSet<String>(List.of("++", "--", "=", "*=", "/=", "%=", "+=", "-=", "<<=", ">>=", "&=", "!=", "^=", ">>>="));
         HashSet<String> operatorsC = new HashSet<String>(List.of("==", "!=", ">=", "<=", ">", "<", "!", "else", "if", "?", "try", "catch", "when"));
         KotlinLexer lexer = new KotlinLexer(new ANTLRFileStream(file.getAbsolutePath()));
@@ -88,21 +90,38 @@ public class Main {
             String prevToken = tokens.get(i - 1).getText();
             String currToken = tokens.get(i).getText();
             String currTokenType = lexer.getVocabulary().getDisplayName(tokens.get(i).getType());
+            String prevTokenType = lexer.getVocabulary().getDisplayName(tokens.get(i - 1).getType());
             if (prevToken.equals("class") && currTokenType.equals("Identifier")) {
                 key = currToken;
                 waitClass = true;
             }
+            if ((currToken.equals("val") || currToken.equals("var") || currToken.equals("const")) && operatorsA.contains(tokens.get(i + 2).getText())) {
+                val = true;
+            }
+            if (operatorsA.contains(currToken) && !val) {
+                a++;
+            }
+            if (val && operatorsA.contains(currToken)) {
+                val = false;
+            }
+            if (currTokenType.equals("Identifier") && tokens.get(i + 1).getText().equals("(") && !prevToken.equals("fun") && !waitClass) {
+                b++;
+            }
+            if (operatorsC.contains(currToken)) {
+                c++;
+            }
+
             if (waitClass && prevToken.equals(":") && currTokenType.equals("Identifier") && tokens.get(i - 2).getText().equals(")")) {
-                value=currToken;
-                waitClass=false;
-                extend.put(key,value);
+                value = currToken;
+                waitClass = false;
+                extend.put(key, value);
             }
-            if (currToken.equals("{") || currToken.equals("=")){
-                waitClass=false;
+            if (currToken.equals("{") || currToken.equals("=")) {
+                waitClass = false;
             }
-                if (currToken.equals("override")) {
-                    override++;
-                }
+            if (currToken.equals("override")) {
+                override++;
+            }
 
             if (currToken.equals("fun")) {
                 fun = true;
@@ -122,22 +141,12 @@ public class Main {
                 fields++;
             }
 
-            if (operatorsC.contains(currToken)) {
-                c++;
-            }
-            if ((currToken.equals("val") || currToken.equals("var") || currToken.equals("const")) && operatorsA.contains(tokens.get(i + 2).getText())) {
-                val = true;
-            }
-            if (operatorsA.contains(currToken) && !val) {
-                a++;
-            }
-            if (val && operatorsA.contains(currToken)) {
-                val = false;
-            }
+
+
             System.out.println("token " + currToken + " tokentype " + currTokenType);
         }
         //printTree(tree, 0, lexer);
-        System.out.println("fields=" + fields + " override=" + override + " a metric=" + a + " c metric=" + c+ " key="+key+" value="+value);
+        System.out.println("fields=" + fields + " override=" + override + " a metric=" + a + " b metric=" + b + " c metric=" + c + " key=" + key + " value=" + value);
     }
 
     public void reader() throws Exception {
